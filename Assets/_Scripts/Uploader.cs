a9e202b0147891e6485bcb2d79c63291d6f7873d
David Montufo <dmontufo@gmail.com>
2022-10-06 14:14:32 +0200

    Revert "Removed legacy unity function"
    This reverts commit 4558a3eb31a54c53f4531e5992aaab5bab5f9029.

--- Assets/_Scripts/Uploader.cs
+++ Assets/_Scripts/Uploader.cs
@@ -8,19 +8,24 @@ public static class Uploader
 {
     public static IEnumerator UploadNewPlayer(string name, string country, DateTime dateTime)
     {
-        List<IMultipartFormSection> form = new List<IMultipartFormSection>();
-        form.Add(new MultipartFormDataSection("name=" + name + "&country=" + country + "&dateTime=" + dateTime.ToString()));
+        WWWForm form = new WWWForm();
+        form.AddField("name", name);
+        form.AddField("country", country);
+        form.AddField("dateTime", dateTime.ToString());
 
-        UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/NewPlayer.php", form);
-        yield return www.SendWebRequest();
+        using (UnityWebRequest www = UnityWebRequest.Post("https://citmalumnes.upc.es/~davidmm24/NewPlayer.php", form))
+        {
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.SendWebRequest();
 
-        if (www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Debug.Log("Form upload complete! Response text from the server = " + www.downloadHandler.text);
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete! Response text from the server = " + www.downloadHandler.text);
+            }
         }
     }
 }
